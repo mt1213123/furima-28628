3d9c46604614099a7f942ba1c48a8447da6aaa05
mt1213123 <mari.orange.tachibana.1996@hotmail.co.jp>
2020-12-13 21:40:56 +0900

    ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_KANANAME_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :kanafamily_name, presence: true, format: { with: VALID_KANANAME_REGEX }
+  validates :kanafirst_name, presence: true, format: { with: VALID_KANANAME_REGEX }
   validates :birth_day, presence: true
 end
