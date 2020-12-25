dd379c373f9c1b001e1fbcf5059a7602caaec8f6
mt1213123 <mari.orange.tachibana.1996@hotmail.co.jp>
2020-12-25 23:13:58 +0900

    アソシエーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
 
   def index
@@ -21,7 +22,6 @@ class ItemsController < ApplicationController
   private 
 
   def item_params
-  
     params.require(:item).permit(:title, :description, :image, :category_id, :condition_id, :prefecture_id, :fee_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
--- db/migrate/20201221054038_create_items.rb
+++ db/migrate/20201221054038_create_items.rb
@@ -4,11 +4,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :title, null:false 
       t.text :description, null:false
       t.integer :price, null:false
-      t.integer :category_id, null:false, foreign_key:true
-      t.integer :condition_id, null:false, foreign_key:true
-      t.integer :prefecture_id, null:false, foreign_key:true
-      t.integer :fee_id, null:false, foreign_key:true
-      t.integer :days_id, null:false, foreign_key:true
+      t.integer :category_id, null:false
+      t.integer :condition_id, null:false
+      t.integer :prefecture_id, null:false
+      t.integer :fee_id, null:false
+      t.integer :days_id, null:false
       t.references :user, null:false, foreign_key:true
       t.timestamps
     end
