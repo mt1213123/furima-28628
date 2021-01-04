2e284cb2d8ac1883ba7ad34224df0ea3487bfaa5
mt1213123 <mari.orange.tachibana.1996@hotmail.co.jp>
2021-01-04 20:26:07 +0900

    商品出品機能revert後

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,27 @@
+class ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user! only: [:new, :create]
+
+  def index
+    #@items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :image, :category_id, :condition_id, :prefecture_id, :fee_id, :days_id, :price).merge(user_id: current_user.id)
+  end
+end
