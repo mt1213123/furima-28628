1c5f6a758b8c93295a541ffe61a295b8bca04c6c
mt1213123 <mari.orange.tachibana.1996@hotmail.co.jp>
2021-01-07 19:12:36 +0900

    修正3

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
-  describe '配送先情報の保存' do
     before do
       @purchase = FactoryBot.build(:purchase)
       @purchase_address = FactoryBot.build(:purchase_address)
     end
-
-    it 'すべての値が正しく入力されていれば保存できること' do
-      expect(@purchase_addresses).to be_valid
-    end
+  
+  describe '配送先情報の保存' do
+   context '配送先情報の保存がうまくいくとき' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@purchase_addresses).to be_valid
+      end
+      it 'buildingは空でも保存できること' do
+        @purchase_address.building = nil
+        expect(@purchase_address).to be_valid
+      end
+   end
+    
+   context '配送先情報の保存がうまくいかないとき' do
     it 'postal_codeが空だと保存できないこと' do
       @purchase_address.postal_code = nil
       @purchase_address.valid?
@@ -35,10 +43,6 @@ RSpec.describe PurchaseAddress, type: :model do
       @purchase_address.valid?
       expect(@purchase_address.errors.full_messages).to include("Block num can't be blank")
     end
-    it 'buildingは空でも保存できること' do
-      @purchase_address.building = nil
-      expect(@purchase_address).to be_valid
-    end
     it 'phone_numが空だと保存できないこと' do
       @purchase_address.phone_num = nil
       @purchase_address.valid?
@@ -59,10 +63,27 @@ RSpec.describe PurchaseAddress, type: :model do
       @purchase_address.valid?
       expect(@purchase_address.errors.full_messages).to include("Phone num is invalid")
     end
+    it 'phone_numが英数字混合であると保存できないこと' do
+      @purchase_address.phone_num = 'aaaaaaaaaa1'
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include("Phone num is invalid")
+    end
     it "tokenが空では登録できないこと" do
       @purchase_address.token = nil
       @purchase_address.valid?
       expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
     end
+    it 'user_idが空だと保存できないこと' do
+      @purchase.user_id = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("User must exist")
+    end
+    it 'item_idが空だと保存できないこと' do
+      @purchase.item_id = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include("Item must exist")
+    end
+   end
+
   end
 end
